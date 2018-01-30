3ad804bcb7e6c8708072a63dbd09f52411ef324f
Alex Wandres <alex.r.wandres@gmail.com>
2018-01-30 06:09:19 -0500

    add MLAB url

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
+const MLAB_URL = 'mongodb://awandres:12Mexmex@ds113825.mlab.com:13825/centennial'
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MLAB_URL)
+  mongoose.connect(MLAB_URL)
 } else {
   mongoose.connect('mongodb://localhost/centennial'), {
   useMongoClient: true
